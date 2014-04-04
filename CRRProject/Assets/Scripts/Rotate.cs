5a6bce41769f5a2b53ea5a87d187b8dd9985ffbc
BenSaus <ben@pickledbrain.co>
2014-04-04 13:30:38 -0600

    Added Test Assets and Folders

---
+++ CRRProject/Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+	void Start ()
+	{
+	
+	}
+
+	void Update () 
+	{
+		transform.Rotate (Vector3.up, 1f);
+	}
+}
